3dc9e506ccee736c2600c165efeb5a53a4dfa2be
Shirel Chale <shirelhale16@gmail.com>
2020-10-20 23:42:44 +0300

    Initial code + Welcome2070

---
+++ dotNet5781_00_2070_5493/Program2070.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_2070_5493
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome2070();
+            Welcome5493();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome5493();
+
+        private static void Welcome2070() 
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
