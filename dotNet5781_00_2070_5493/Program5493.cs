0c196ffe99397da4051ce4ac95b8b2fe161c3fad
Michal Abargel <mbrgl715@gmail.com>
2020-10-21 21:43:34 +0300

    Intial code + welcome5493

---
+++ dotNet5781_00_2070_5493/Program5493.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_2070_5493
+{
+    partial class Program
+    {
+      
+      static partial void Welcome5493()
+        {
+            Console.WriteLine("I am also here");
+        }
+    }
+}
